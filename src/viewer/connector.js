5edb735d63f71d607d7f19499735d957a3c66db2
Steve Pieper <pieper@isomics.com>
2017-08-20 14:55:00 -0400

    WIP: pre-load dependencies for serving from github

--- src/viewer/connector.js
+++ src/viewer/connector.js
@@ -1,21 +1,23 @@
 var Connector = (function () {
-  var mockUrl = 'http://localhost:4000/mock.json';
+  const mockData = {
+    "name": "default case",
+    "urls": [
+      "https://s3.amazonaws.com/IsomicsPublic/SampleData/MRHead/oneseries/1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.366.0.dcm",
+      "https://s3.amazonaws.com/IsomicsPublic/SampleData/MRHead/oneseries/1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.367.0.dcm",
+      "https://s3.amazonaws.com/IsomicsPublic/SampleData/MRHead/oneseries/1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.368.0.dcm",
+
+    /*
+      "https://s3.amazonaws.com/IsomicsPublic/SampleData/MRHead/oneseries/1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.369.0.dcm",
+      "https://s3.amazonaws.com/IsomicsPublic/SampleData/MRHead/oneseries/1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.370.0.dcm",
+      "https://s3.amazonaws.com/IsomicsPublic/SampleData/MRHead/oneseries/1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.371.0.dcm"
+      */
+    ]
+  };
 
   return {
     getCase: function (callback) {
-      var successHandler = function (response) {
-        callback(null, response);
-      };
-
-      var errorHandler = function (error) {
-        if (error) {
-          console.error(error);
-        }
-
-        callback(error);
-      };
-
-      $.ajax(mockUrl).done(successHandler).fail(errorHandler);
+      callback(null, mockData);
     }
   }
+
 })();
